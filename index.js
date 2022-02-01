65b32cc22f6649cf0a98a06d2db1fabc1724ed88
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-02-01 13:07:04 +0100

    Refactor code to use Jquery

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const list = document.querySelectorAll('.navigation li');
+$(function () {
+  const list = document.querySelectorAll('.navigation li');
 
-const activeLink = () => {
-  list.forEach((item) => item.classList.remove('hovered'));
-  this.classList.add('hovered');
-};
-list.forEach((item) => item.addEventListener('mouseover', activeLink));
+  const activeLink = () => {
+    list.forEach((item) => item.classList.remove('hovered'));
+    this.classList.add('hovered');
+  };
+  list.forEach((item) => item.addEventListener('mouseover', activeLink));
 
-const navigation = document.querySelector('.navigation');
-const main = document.querySelector('.main');
+  const navigation = document.querySelector('.navigation');
+  const main = document.querySelector('.main');
 
-$('.toggle').on('click', () => {
-  navigation.classList.toggle('active');
-  main.classList.toggle('active');
+  $('.toggle').on('click', () => {
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+  });
 });
